e0997e1047fb2c5bc854ed48579ebb5c11e434ed
ihor.prokofiev <dar.dhan@gmail.com>
2020-02-17 15:27:09 +0200

    fixes

--- src/pages/practice-settings/practice-settings.ts
+++ src/pages/practice-settings/practice-settings.ts
@@ -68,9 +68,9 @@ export class PracticeSettingsPage {
       this.practiceDuration = this.transformTime(this.settings.practiceDuration, 'T->S');
     }
 
-    if (!this.settings.intervals || !this.settings.intervals.length ) {
-      this.settings.intervals = [{value:0}]
-    }
+    // if (!this.settings.intervals || !this.settings.intervals.length ) {
+    //   this.settings.intervals = [{value:0}]
+    // }
   }
 
   setExDurationsByDefault () {
--- src/pipe/msToHours.ts
+++ src/pipe/msToHours.ts
@@ -1,15 +1,21 @@
 // timespan in milliseconds to hours
 
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
-@Pipe({name: 'msToHours'})
+@Pipe({ name: 'msToHours' })
 export class MsToHoursPipe implements PipeTransform {
     transform(value: any): any {
         if (!value) {
             return null;
         }
-        const minutes = Math.floor((value / (1000 * 60)) % 60);
-        const hours = Math.floor((value / (1000 * 60 * 60)) % 24);
+
+        let seconds = value / 1000;        
+        let hours = parseInt(seconds / 3600);
+        seconds = seconds % 3600;
+        let minutes = parseInt(seconds / 60);
+        seconds = seconds % 60;
+
         return `${hours}:${minutes}`;
     }
 }
